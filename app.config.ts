b4849cb8d1875d229f43493c2010bbdfac2b96cc
Sudo-Self <sudo-self@proton.me>
2023-12-16 09:32:21 -0700

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,12 @@
 export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
-
   socials: {
-    twitter: 'ilosmyipad',
     github: 'sudo-self/content-wind',
   },
 
   nuxtIcon: {
     iconifyApiOptions: {
-      publicApiFallback: true
     },
 
     class: 'tailwind.css'

26549a85406758ffba818dc3627a0ff6a9f489b4
Sudo-Self <sudo-self@proton.me>
2024-01-11 02:07:09 -0700

    asd
    asd

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
   socials: {
-    github: 'https://github.com/sudo-self/'
+    github: ''
   },
 
   nuxtIcon: {
